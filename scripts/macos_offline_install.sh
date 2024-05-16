e4deeb8809bcab2ae7d7c6441c98986b27ab7bf4
wander <wksw123456@gmail.com>
2024-05-16 18:18:42 +0800

    update script

--- scripts/macos_offline_install.sh
+++ scripts/macos_offline_install.sh
@@ -49,23 +49,23 @@ provision:
 - mode: system
   script: |
     [ -f /init-done ] && exit 0
-    cat << EOF > /etc/apt/sources.list
-    # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-    deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
-    # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
-    deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
-    # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
-    deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
-    # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+    # cat << EOF > /etc/apt/sources.list
+    # # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+    # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+    # # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+    # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+    # # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+    # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+    # # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
 
-    # 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
-    deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-    # deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+    # # 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
+    # deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+    # # deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
 
-    # 预发布软件源，不建议启用
-    # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
-    # # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
-    EOF
+    # # 预发布软件源，不建议启用
+    # # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+    # # # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+    # EOF
 
     sudo apt-get update && sudo apt-get install -y ansible python3 wget
     sudo apt-get install -y python3-pip
